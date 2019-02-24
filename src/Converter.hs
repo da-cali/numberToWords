a84d3a515351c4c55549b4bcb7c28012056f3722
Dan <dan@localhost.localdomain>
2019-02-23 20:12:11 -0800

    code readability improved

--- src/Converter.hs
+++ src/Converter.hs
@@ -2,7 +2,7 @@ module Converter
 
 ( toWords
 ) where
-        
+
 import Data.List
 
 -- Converter
@@ -35,7 +35,7 @@ toWords 60 = "sixty"
 toWords 70 = "seventy"
 toWords 80 = "eighty"
 toWords 90 = "ninety"
-toWords n = composer bigNumbers 3 where
+toWords n = composer bigNumbers 6 where
   composer (x:xs) p
     | n < 0 = "minus " 
             ++ toWords (-n)
@@ -46,6 +46,11 @@ toWords n = composer bigNumbers 3 where
                ++ " hundred" 
                ++ if n `mod` 100 == 0 then "" else " " 
                ++ toWords (n `mod` 100)
+    | n < 1000000 = toWords (n `div` 1000) 
+                  ++ " " 
+                  ++ "thousand" 
+                  ++ if n `mod` 1000 == 0 then "" else ", " 
+                  ++ toWords (n `mod` 1000)
     | n < 10^(p+3) = toWords (n `div` 10^p) 
                    ++ " " 
                    ++ x 
@@ -53,26 +58,27 @@ toWords n = composer bigNumbers 3 where
                    ++ toWords (n `mod` 10^p)
     | otherwise = composer xs (p+3)
 
--- Names of large numbers
+
+-- Names of large numbers (million, billion, trillion, ...)
 bigNumbers :: [String]
-bigNumbers = "thousand" : map (++ "illion") (from1to999 ++ from1000toInf) where
-  from1to999 = us ++ ts ++ hs
-  us = ["m","b","tr","quadr","quint","sext","sept","oct","non"]
-  ts = foldl' (\ acc x -> acc ++ x : map (combine x) uPxs) [] tPxs
-  hs = foldl' (\ acc x -> acc ++ x : map (combine x) uPlusTpxs) [] hPxs
-  uPlusTpxs = let addVow w | any (`isInfixOf` w) (drop 2 tPxs) = w ++ "a"
-                           | otherwise = w ++ "i"
-              in uPxs ++ map addVow ts
-  combine sx px = let ix | elem sx sExceps && px == "se" = "s"
-                         | elem sx xExceps && px == "se" = "x"
-                         | elem sx mExceps && elem px ["septe","nove"] = "m"
-                         | elem sx nExceps && elem px ["septe","nove"] = "n"
-                         | elem sx (sExceps ++ xExceps) && px == "tre" = "s"
-                         | otherwise = ""
-                  in px ++ ix ++ sx
-  from1000toInf = concatMap bigPxs [1..]
-  bigPxs n = let bigs = map (++ (concat $ replicate n "ill") ++ "i") from1to999
-             in foldl' (\ acc x -> acc ++ map (x++) ("n" : from1to999)) [] bigs
+bigNumbers = map (++ "illion") (from1to999 ++ from1000toInf) where
+  from1to999 = us ++ ts ++ hs where
+    us = ["m","b","tr","quadr","quint","sext","sept","oct","non"]
+    ts = foldl' (\ acc x -> acc ++ x : map (addIfx x) uPxs) [] tPxs
+    hs = foldl' (\ acc x -> acc ++ x : map (addIfx x) uPxsWithTpxs) [] hPxs
+    uPxsWithTpxs = let addVow w | any (`isInfixOf` w) (drop 2 tPxs) = w ++ "a"
+                                | otherwise = w ++ "i"
+                   in uPxs ++ map addVow ts
+    addIfx sx px = let ix | elem sx sExceps && px == "se" = "s"
+                          | elem sx xExceps && px == "se" = "x"
+                          | elem sx mExceps && elem px ["septe","nove"] = "m"
+                          | elem sx nExceps && elem px ["septe","nove"] = "n"
+                          | elem sx (sExceps ++ xExceps) && px == "tre" = "s"
+                          | otherwise = ""
+                   in px ++ ix ++ sx
+  from1000toInf = concatMap bigPxs [1..] where
+    bigPxs n = let bigs = map (++ (concat $ replicate n "ill") ++ "i") from1to999
+               in foldl' (\ acc x -> acc ++ map (x++) ("n" : from1to999)) [] bigs
 
 -- Units' prefixes
 uPxs :: [String]
--- test/Spec.hs
+++ test/Spec.hs
@@ -6,7 +6,10 @@ main = do
     print(toWords (-99) == "minus ninety-nine")
     print(toWords 12345 == "twelve thousand, three hundred forty-five")
     print(toWords (10^165) == "one quattuorquinquagintillion")
+    print(toWords (10^1002) == "one trestrigintatrecentillion")
+    print(toWords (10^3000) == "one novenonagintanongentillion")
     print(toWords (10^3003) == "one millinillion")
     print(toWords (10^3705) == "one milliquattuortrigintaducentillion")
+    print(toWords (10^19683) == "one sextillisexagintaquingentillion")
     print(toWords (10^59052) == "one novendecillitresoctogintasescentillion")
     
