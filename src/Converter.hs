f3e315a1effc2bd7315629505dc208933ad7c0a8
Dan <dan@myemail.com>
2021-02-22 00:19:57 -0800

    code formatting

--- src/Converter.hs
+++ src/Converter.hs
@@ -32,47 +32,52 @@ nameOf 60 = "sixty"
 nameOf 70 = "seventy"
 nameOf 80 = "eighty"
 nameOf 90 = "ninety"
-nameOf n 
-  | n < 0 = "minus " ++ nameOf (-n)
-  | n < 100 = nameOf (n - n`mod`10) ++ '-' : nameOf (n`mod`10)
-  | n < 1000 = nameOf (n`div`100) ++ " hundred" ++ if n`mod`100 == 0 then "" else " " ++ nameOf (n`mod`100)
-  | otherwise = getNameFrom pairsOfNamesAndPowers where
-      pairsOfNamesAndPowers = zip bigNumbers [3,6..]
-      getNameFrom ((w,p):theRest) | n >= 10^(p+3) = getNameFrom theRest
-                                  | otherwise = nameOf (n`div`10^p) ++ ' ' : w
-                                                                    ++ if n`mod`10^p == 0 then "" else ", "
-                                                                    ++ nameOf (n`mod`10^p)
-
+nameOf n | n < 0 = "minus " ++ nameOf (-n)
+         | n < 100 = nameOf (n - n`mod`10) ++ '-' : nameOf (n`mod`10)
+         | n < 1000 = nameOf (n`div`100) ++ " hundred" ++ nameOfRemainder
+         | otherwise = getNameFrom labeldPowers where
+             nameOfRemainder = if n`mod`100 == 0 then "" else " " ++ nameOf (n`mod`100)
+             labeldPowers = zip bigNumbers [3,6..]
+             getNameFrom ((l,p):rest) = if n >= 10^(p+3) 
+                                            then getNameFrom rest
+                                            else nameOf (n`div`10^p) 
+                                              ++ ' ' : l
+                                              ++ if n`mod`10^p == 0 then "" else ", "
+                                              ++ nameOf (n`mod`10^p)
+                            
 -- Names of powers of 10 that are multiples of 3 (thousand, million, billion..)
 bigNumbers :: [String]
-bigNumbers = "thousand" : map (++ "illion") (from1to999 ++ from1000toInf) where
-  -- First 999 prefixes (from "m" to "novenonagintanongent").
-  from1to999 = let
-    -- Units', tenths', and hundreds' prefixes.
-    uPfxs = ["un","duo","tre","quattuor","quin","se","septe","octo","nove"]
-    tPfxs = ["dec","vigint","trigint","quadragint","quinquagint","sexagint","septuagint","octogint","nonagint"]
-    hPfxs = ["cent","ducent","trecent","quadringent","quingent","sescent","septingent","octingent","nongent"]
-    -- Exceptions for "s", "x", "m", and "n".
-    sExceps = ["vigint","trigint","quadragint","quinquagint","trecent","quadringent","quingent"]
-    xExceps = ["octogint","cent","octingent"]
-    mExceps = ["vigint","octogint","octingent"]
-    nExceps = filter (`notElem` "nonagint":"nongent":mExceps) (tPfxs ++ hPfxs)
-    -- Helper functions.
-    addVowel w = w ++ if any (`isInfixOf` w) (drop 2 tPfxs) then "a" else "i"
-    addInfix sx px = px ++ ix ++ sx where
-      ix | px`elem`["septe","nove"] && sx`elem`nExceps = "n"
-         | px`elem`["septe","nove"] && sx`elem`mExceps = "m"
-         | px=="tre" && sx`elem`(sExceps++xExceps) = "s"
-         | px=="se" && sx`elem`sExceps = "s"
-         | px=="se" && sx`elem`xExceps = "x"
-         | otherwise = ""
-    -- Combined prefixes.
-    us = ["m","b","tr","quadr","quint","sext","sept","oct","non"]
-    ts = concatMap (\ x -> x : map (addInfix x) uPfxs) tPfxs
-    hs = concatMap (\ x -> x : map (addInfix x) (uPfxs ++ map addVowel ts)) hPfxs
-    in us ++ ts ++ hs
-  -- Rest of the prefixes (from "millin" to infinity).
-  from1000toInf = let
-    bigPfxs n = foldl' (\ acc x -> acc ++ map (x++) ("n" : from1to999)) [] bigs
-      where bigs = map (++ (concat $ replicate n "ill") ++ "i") from1to999
-    in concatMap bigPfxs [1..]
+bigNumbers = "thousand" : fmap (++ "illion") (from1to999 ++ from1000toInf) where
+    -- First 999 prefixes (from "m" to "novenonagintanongent").
+    from1to999 =
+        let -- Units', tenths', and hundreds' prefixes.
+            uPfxs = ["un","duo","tre","quattuor","quin","se","septe","octo","nove"]
+            tPfxs = ["dec","vigint","trigint","quadragint","quinquagint"
+                    ,"sexagint","septuagint","octogint","nonagint"]
+            hPfxs = ["cent","ducent","trecent","quadringent","quingent"
+                    ,"sescent","septingent","octingent","nongent"]
+            -- Exceptions for "s", "x", "m", and "n".
+            xExceps = ["octogint","cent","octingent"]
+            mExceps = ["vigint","octogint","octingent"]
+            sExceps = ["vigint","trigint","quadragint","quinquagint"
+                      ,"trecent","quadringent","quingent"]
+            nExceps = filter (`notElem` "nonagint":"nongent":mExceps) (tPfxs ++ hPfxs)
+            -- Helper functions.
+            addVowel w = w ++ if any (`isInfixOf` w) (drop 2 tPfxs) then "a" else "i"
+            addInfix sx px = px ++ ix ++ sx
+                where ix | px`elem`["septe","nove"] && sx`elem`nExceps = "n"
+                         | px`elem`["septe","nove"] && sx`elem`mExceps = "m"
+                         | px=="tre" && sx`elem`(sExceps++xExceps) = "s"
+                         | px=="se" && sx`elem`sExceps = "s"
+                         | px=="se" && sx`elem`xExceps = "x"
+                         | otherwise = ""
+            -- Combined prefixes.
+            us = ["m","b","tr","quadr","quint","sext","sept","oct","non"]
+            ts = concatMap (\ x -> x : fmap (addInfix x) uPfxs) tPfxs
+            hs = concatMap (\ x -> x : fmap (addInfix x) (uPfxs ++ map addVowel ts)) hPfxs
+        in us ++ ts ++ hs
+    -- Rest of the prefixes (from "millin" to infinity).
+    from1000toInf =
+        let bigPfxs n = foldl' (\ acc x -> acc ++ fmap (x++) ("n" : from1to999)) [] bigs
+                where bigs = map (++ (concat $ replicate n "ill") ++ "i") from1to999
+        in concatMap bigPfxs [1..]
--- test/Spec.hs
+++ test/Spec.hs
@@ -2,14 +2,15 @@ import Converter
 
 main :: IO ()
 main = do
-    print (nameOf 0 == "zero")
-    print (nameOf (-99) == "minus ninety-nine")
-    print (nameOf 12345 == "twelve thousand, three hundred forty-five")
-    print (nameOf (10^165) == "one quattuorquinquagintillion")
-    print (nameOf (10^1002) == "one trestrigintatrecentillion")
-    print (nameOf (10^3000) == "one novenonagintanongentillion")
-    print (nameOf (10^3003) == "one millinillion")
-    print (nameOf (10^3705) == "one milliquattuortrigintaducentillion")
-    print (nameOf (10^19683) == "one sextillisexagintaquingentillion")
-    print (nameOf (10^59052) == "one novendecillitresoctogintasescentillion")
-    
+    mapM_ print [ nameOf 0 == "zero"
+                , nameOf (-99) == "minus ninety-nine"
+                , nameOf 12345 == "twelve thousand, three hundred forty-five"
+                , nameOf 200000 == "two hundred thousand"
+                , nameOf (10^165) == "one quattuorquinquagintillion"
+                , nameOf (10^1002) == "one trestrigintatrecentillion"
+                , nameOf (10^3000) == "one novenonagintanongentillion"
+                , nameOf (10^3003) == "one millinillion"
+                , nameOf (10^3705) == "one milliquattuortrigintaducentillion"
+                , nameOf (10^19683) == "one sextillisexagintaquingentillion"
+                , nameOf (10^59052) == "one novendecillitresoctogintasescentillion"
+                ]
